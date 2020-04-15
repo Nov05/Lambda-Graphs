6c0ff984b8e48bf4f3a14b4374b94f8117bafd6f
nov05 <arwen.liuwenjing@gmail.com>
2020-04-14 20:40:50 -0500

    2020-04-14

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -33,15 +33,14 @@ def earliest_ancestor(ancestors, starting_node):
 
 
 if __name__ == '__main__':
-    '''
+    print('''
        10
      /
     1   2   4  11
      \ /   / \ /
       3   5   8
-       \ / \   \
-        6   7   9
-    '''
+       \ / \   \\
+        6   7   9''')
     ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
     earliest_ancestor(ancestors, 1)
     earliest_ancestor(ancestors, 6)
